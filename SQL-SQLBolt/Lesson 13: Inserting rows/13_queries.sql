c039dbd204b871061ea398381709e472d75a6aad
E. Lim <88494428+kuehbiko@users.noreply.github.com>
2023-04-10 14:49:32 +0800

    Create 13_queries.sql

---
+++ SQL-SQLBolt/Lesson 13: Inserting rows/13_queries.sql
@@ -0,0 +1,11 @@
+-- SQL Lesson 13: Inserting rows
+
+-- Add the studio's new production, Toy Story 4 to the list of movies (you can use any director)
+INSERT INTO movies
+(title, director, year, length_minutes)
+VALUES ('Toy Story 4', 'Pete Docter', 2022, 100 );
+
+-- Toy Story 4 has been released to critical acclaim! It had a rating of 8.7, and made 340 million domestically and 270 million internationally. 
+-- Add the record to the BoxOffice table.
+INSERT INTO boxoffice
+VALUES (4, 8.7, 340*1000000, 270*1000000);
