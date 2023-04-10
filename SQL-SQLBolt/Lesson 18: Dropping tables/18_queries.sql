3599b206746395d6ebad9e104d83587872da7b5a
E. Lim <88494428+kuehbiko@users.noreply.github.com>
2023-04-10 14:53:14 +0800

    Create 18_queries.sql

---
+++ SQL-SQLBolt/Lesson 18: Dropping tables/18_queries.sql
@@ -0,0 +1,8 @@
+-- SQL Lesson 18: Dropping tables
+
+-- We've sadly reached the end of our lessons, lets clean up by removing the Movies table
+
+DROP TABLE IF EXISTS movies;
+
+-- And drop the BoxOffice table as well
+DROP TABLE IF EXISTS boxoffice;
