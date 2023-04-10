895da307a7893e53f033b64939150d4f89eb487c
E. Lim <88494428+kuehbiko@users.noreply.github.com>
2023-04-10 13:49:17 +0800

    Create 586. Customer Placing the Largest Number of Orders.sql

---
+++ SQL-LeetCode/SQL I/08-Function/586. Customer Placing the Largest Number of Orders.sql
@@ -0,0 +1,8 @@
+-- 586. Customer Placing the Largest Number of Orders
+-- Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
+-- The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+
+SELECT customer_number FROM orders
+GROUP BY customer_number
+ORDER BY COUNT(order_number) DESC
+LIMIT 1;
