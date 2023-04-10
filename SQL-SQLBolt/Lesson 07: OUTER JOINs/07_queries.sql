bdf3c3468c98885aa003268a983c1ab35e635aec
E. Lim <88494428+kuehbiko@users.noreply.github.com>
2023-04-10 14:35:19 +0800

    Update 07_queries.sql

--- SQL-SQLBolt/Lesson 07: OUTER JOINs/07_queries.sql
+++ SQL-SQLBolt/Lesson 07: OUTER JOINs/07_queries.sql
@@ -1,1 +1,15 @@
+-- SQL Lesson 7: OUTER JOINs
 
+-- Find the list of all buildings that have employees
+SELECT DISTINCT building_name FROM buildings
+LEFT JOIN employees
+ON building_name = building
+WHERE name IS NOT NULL;
+
+-- Find the list of all buildings and their capacity
+SELECT * FROM buildings;
+
+-- List all buildings and the distinct employee roles in each building (including empty buildings)
+SELECT DISTINCT building_name, role FROM buildings
+LEFT JOIN employees
+ON building_name = building;
