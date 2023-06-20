845c2ab4177b2272a9081ab2d52df9f9bde5ef6a
E. Lim <88494428+kuehbiko@users.noreply.github.com>
2023-06-20 23:46:52 +0800

    Update Challenge_01: Book List Database.sql

--- SQL-Khan Academy/Challenge_01: Book List Database.sql
+++ SQL-Khan Academy/Challenge_01: Book List Database.sql
@@ -1,13 +1,15 @@
-CREATE TABLE book_list (
-"id" NUMERIC,
-"name" TEXT,
-"author" TEXT,
-"rating" NUMERIC
-);
+CREATE TABLE book_list 
+  (
+  "id" NUMERIC,
+  "name" TEXT,
+  "author" TEXT,
+  "rating" NUMERIC
+  );
 
-INSERT INTO book_list ("id", "name", "author", "rating")
+INSERT INTO book_list 
+  ("id", "name", "author", "rating")
 VALUES
-("1", "The Magician's Nephew", "CS Lewis", "4"),
-("2", "A Wrinkle in Time", "Madaleine L'Engle", "5"),
-("3", "QED", "Richard Feynman", "3")
+  ("1", "The Magician's Nephew", "CS Lewis", "4"),
+  ("2", "A Wrinkle in Time", "Madaleine L'Engle", "5"),
+  ("3", "QED", "Richard Feynman", "3")
 ;
