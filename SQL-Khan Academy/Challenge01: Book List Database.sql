c8da6e22e3a34db0a3570b5e8d5b103ed45f6df3
E. Lim <88494428+kuehbiko@users.noreply.github.com>
2023-06-20 23:54:06 +0800

    Update and rename Challenge_01: Book List Database.sql to Challenge01: Book List Database.sql

--- SQL-Khan Academy/Challenge01: Book List Database.sql
+++ SQL-Khan Academy/Challenge01: Book List Database.sql
@@ -1,15 +1,9 @@
 CREATE TABLE book_list 
-  (
-  "id" NUMERIC,
-  "name" TEXT,
-  "author" TEXT,
-  "rating" NUMERIC
-  );
+  ("id" NUMERIC, "name" TEXT, "author" TEXT, "rating" NUMERIC);
 
 INSERT INTO book_list 
   ("id", "name", "author", "rating")
 VALUES
   ("1", "The Magician's Nephew", "CS Lewis", "4"),
   ("2", "A Wrinkle in Time", "Madaleine L'Engle", "5"),
-  ("3", "QED", "Richard Feynman", "3")
-;
+  ("3", "QED", "Richard Feynman", "3");
